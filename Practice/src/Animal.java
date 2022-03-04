2597f3830a2ecd13fb6c7b91a83da44667177de6
Matt <mglad776@gmail.com>
2022-03-04 13:29:18 -0500

    Java Practice

---
+++ Practice/src/Animal.java
@@ -0,0 +1,25 @@
+package Practice.src;
+
+public class Animal {
+    
+    public static String iAmDog(){
+        return "I am a dog";
+    }
+
+    public static void doStuff(){
+        int a = 5;
+        // if (a == 0){
+
+        // } else if(a == 1){
+
+        // } else{
+
+        // }
+
+        for (int i  =  0; i < 5; i++){
+            System.out.println(i);
+
+        }
+    }
+    
+}
---
+++ Practice/src/App.java
@@ -0,0 +1,21 @@
+package Practice.src;
+
+// import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        
+        //  String exclaim = addExclamationPoint("hot dogs");
+        //  System.out.println(exclaim);
+         Animal a = new Animal();
+        // String dog = a.iAmDog();
+        // System.out.println(dog);
+        // ArrayList<Integer> a = new ArrayList<Integer>();
+        a.doStuff();
+    
+    }
+    // public static String addExclamationPoint(String s){
+        // return s + "!";
+    // }
+
+}
